f97b05d5245d38702fecee52e1505ef01cb2cf27
javiergp23 <javgon2306@gmail.com>
2026-09-13 19:14:54 -0300

    signout

--- routes/signout.js
+++ routes/signout.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-    res.send("Signout")
+router.post("/", (req, res) => {
+    const {username, name, password} = req.body;
+    if(!!username && !!name && !!password){
+        return res.status(400).json(
+            jsonResponse(400, {
+                error: "Missing required fields"
+            })
+        )
+    }
+    res.send("signout")
 })
 
 module.exports = router;
--- routes/sigup.js
+++ routes/sigup.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-    res.send("Signup")
+router.post("/", (req, res) => {
+    
 })
 
 module.exports = router;
