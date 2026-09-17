7a90da118a38551bd784e54750e6d4cb40e6230c
javiergp23 <javgon2306@gmail.com>
2026-09-17 18:13:46 -0300

    fix signup

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/signup", require("./routes/sigup"));
+app.use("/api/signup", require("./routes/signup"));
 app.use("/api/login", require("./routes/login"));
 app.use("/api/signout", require("./routes/signout"));
 app.use("/api/refreshToken", require("./routes/refreshToken"));
@@ -18,7 +18,7 @@ app.use("/api/user", require("./routes/user"));
 app.use("/api/todos", require("./routes/todos"));
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
+    res.send("Welcome to Node JS API")
 })
 
 app.listen(port, ()=>{
