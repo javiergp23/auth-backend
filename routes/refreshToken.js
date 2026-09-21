14fbc3a94b92a18ca36874324148f1f0c74a21be
javiergp23 <javgon2306@gmail.com>
2026-09-20 22:04:49 -0300

    refeshToken

--- routes/refreshToken.js
+++ routes/refreshToken.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-    res.send("Refresh Token")
+router.get("/", (res) => {
+    res.send("Send Refesh Token")
 })
 
 module.exports = router;
