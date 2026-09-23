8ac2d142d651430e9ba31876be84e2c8e9db0ce4
javiergp23 <javgon2306@gmail.com>
2026-09-22 22:30:52 -0300

    add schema

---
+++ schema/user.js
@@ -0,0 +1,18 @@
+const UserSchema = new mongoose.Schema({
+    id: { type: Number, required: true, unique: true },
+    name: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+export default mongoose.model("User", UserSchema);
