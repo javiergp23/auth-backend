1b30dedbf794fa36e0519f20312eb259fd974eb7
javiergp23 <javgon2306@gmail.com>
2026-09-22 22:32:15 -0300

    add UseSchema

--- schema/user.js
+++ schema/user.js
@@ -1,3 +1,5 @@
+const {Mongoose} = require("mongoose");
+
 const UserSchema = new mongoose.Schema({
     id: { type: Number, required: true, unique: true },
     name: {
@@ -13,6 +15,25 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+});
+
+UserSchema.pre('save', function (next) {
+    const user = this;
+    if (!user.isModified('password')) {
+        return next();
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            return next(err);
+        }
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) {
+                return next(err);
+            }
+            user.password = hash;
+            next();
+        });
+    });
 });
 
 export default mongoose.model("User", UserSchema);
